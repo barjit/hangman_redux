749e53089bee6a851675157ac21fb432bf7b1948
Jeffrey Bartels <jeffreybartels@gmail.com>
2017-07-24 21:27:31 +1000

    add test for read method in TextReader class

--- test/text_reader_test.rb
+++ test/text_reader_test.rb
@@ -2,4 +2,12 @@ require 'minitest/autorun'
 require_relative '../lib/text_reader'
 
 class TextReaderTest < Minitest::Test
+  def setup
+    @text_reader = TextReader.new
+    @filename = '5desk.txt'
+  end
+
+  def test_that_read_method_converts_supplied_text_to_array
+    assert @text_reader.read(@filename).class is_a? Array
+  end
 end
