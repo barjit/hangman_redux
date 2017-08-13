28a2d3e52a6f518cec87299166abd47ece0db192
Jeffrey Bartels <jeffreybartels@gmail.com>
2017-08-13 18:16:52 +1000

    add computer model and tests

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,2 +1,21 @@
 class Computer
+  attr_accessor :user_input
+
+  def get_input
+    until valid?(user_input)
+      puts "Please enter a valid guess: "
+      @user_input = gets.chomp
+    end
+    @user_input
+  end
+
+  def valid?(user_input)
+    alphabet = %w{a b c d e f g h i j k l m n o p q r s t u v w x y z
+                  A B C D E F G H I J K L M N O P Q R S T U V W X Y Z}
+    if alphabet.include? user_input
+      true
+    else
+      false
+    end
+  end
 end
--- tests/computer_test.rb
+++ tests/computer_test.rb
@@ -2,4 +2,19 @@ require 'minitest/autorun'
 require_relative '../lib/computer'
 
 class ComputerTest < Minitest::Test
+  def setup
+    @computer = Computer.new
+    @non_valid_array = ["pop", 32, 12, "^*4", "twelve"]
+    @valid_array = ["a", "g", "p", "o", "g", "w", "f", "T", "L", "n", "Q"]
+  end
+
+  def test_user_input_valid
+    @computer.user_input = @valid_array.sample
+    assert @computer.valid?(@computer.user_input)
+  end
+
+  def test_user_input_not_valid
+    @computer.user_input = @non_valid_array.sample
+    refute @computer.valid?(@computer.user_input)
+  end
 end
