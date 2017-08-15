034933c4477732bec099db8f538345a2c3f38a68
Jeffrey Bartels <jeffreybartels@gmail.com>
2017-08-15 21:37:41 +1000

    edit generate_secret_word method contents
    Edited the method contents so that no line was longer than the
    recommended 80 characters.

--- lib/random_word_generator.rb
+++ lib/random_word_generator.rb
@@ -2,6 +2,9 @@ class RandomWordGenerator
   attr_reader :secret_word
 
   def generate_secret_word(words)
-    @secret_word = words.select {|word| word if word.length > 5 && word.length < 12}.sample
+    @secret_word = words.select do |word|
+      word if word.length > 5 && word.length < 12
+    end
+    @secret_word.sample
   end
 end
