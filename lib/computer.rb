4f9ea51f83cd4de52cf9fc36ce09af1a0208e46a
Jeffrey Bartels <jeffreybartels@gmail.com>
2017-08-17 21:25:09 +1000

    Add methods that return messages and check response
    Add methods that can be called to return a message to the console, such
    as the initial welcome message and notifications for winners and losers.
    Also add method that checks user input for a save/quit request or a
    guess.

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,21 +1,53 @@
+require_relative './save_game'
+
 class Computer
   attr_accessor :user_input
 
   def get_input
+    puts "Please enter a valid guess: "
+    @user_input = gets.chomp
     until valid?(user_input)
       puts "Please enter a valid guess: "
       @user_input = gets.chomp
+      save_quit_or_play(@user_input)
     end
     @user_input
   end
 
   def valid?(user_input)
-    alphabet = %w{a b c d e f g h i j k l m n o p q r s t u v w x y z
-                  A B C D E F G H I J K L M N O P Q R S T U V W X Y Z}
-    if alphabet.include? user_input
+    valid_answers = %w{a b c d e f g h i j k l m n o p q r s t u v w x y z
+                      save quit}
+    if valid_answers.include? user_input
       true
     else
       false
     end
+  end
+
+  def save_quit_or_play(user_input)
+    case user_input
+    when "save"
+      SaveGame.call
+    when "quit"
+      puts "Quitting game, bye!"
+      exit
+    else
+      user_input
+    end
+  end
+
+  def welcome_message
+    puts " ------------------------------------------------------------------- "
+    puts " Welcome to Hangman, try to guess the mystery word before your turns "
+    puts " run out. "
+    puts " ------------------------------------------------------------------- "
+  end
+
+  def game_over_loser_message
+    "You're out of turns, you lose."
+  end
+
+  def game_over_winner_message
+    "That's right, you win!"
   end
 end
