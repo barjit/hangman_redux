5367a3d054fbb86db3bc30f98909dd7dba748588
Jeffrey Bartels <jeffreybartels@gmail.com>
2017-08-17 21:32:27 +1000

    Refactor RandomWordGeneratorClass
    The class didn't need a seperate generate_secret_word method as this
    task could be accomplished when initialized.

--- lib/random_word_generator.rb
+++ lib/random_word_generator.rb
@@ -1,10 +1,10 @@
 class RandomWordGenerator
   attr_reader :secret_word
 
-  def generate_secret_word(words)
+  def initialize(words)
     @secret_word = words.select do |word|
       word if word.length > 5 && word.length < 12
     end
-    @secret_word.sample
+    @secret_word = @secret_word.sample
   end
 end
