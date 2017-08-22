0da91159805dcfdffda26556d090de3ff75a2d8c
Jeffrey Bartels <jeffreybartels@gmail.com>
2017-08-22 20:51:04 +1000

    Add method to show turns left and print a line

--- lib/blackboard.rb
+++ lib/blackboard.rb
@@ -5,16 +5,30 @@ class Blackboard
     @revealed_word = Array.new(secret_word.length) { "_" }
   end
 
-  def show_revealed_word(revealed_word, guess_history)
+  def show_revealed_word(revealed_word, guess_history, turns)
+    print_line
+    puts
     revealed_word.each do |letter|
       print letter + " "
     end
+    print_line
     show_guess_history(guess_history)
     print "\n"
+    show_turns_left(turns)
   end
 
   def show_guess_history(guess_history)
     print "\t\t Guess History: #{guess_history}"
   end
 
+  def show_turns_left(turns)
+    puts
+    puts "You have #{turns} turns left."
+    puts
+  end
+
+  def print_line
+    puts
+    puts "____________________________________________"
+  end
 end
