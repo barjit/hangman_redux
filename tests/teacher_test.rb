a15bbbecf043752d82a50b0ab1a3bcddf367c218
Jeffrey Bartels <jeffreybartels@gmail.com>
2017-08-22 19:49:31 +1000

    Remove test and add a test
    Remove test that returned the result to stdout as this is no longer the
    Teacher class's responsibility (see Blackboard), added a test to make
    sure the store_history method was working as expected.

--- tests/teacher_test.rb
+++ tests/teacher_test.rb
@@ -8,10 +8,15 @@ class TeacherTest < Minitest::Test
     @secret_word = "poppongene"
     @revealed_word = Array.new(@secret_word.length) { "_" }
     @user_input = "p"
+    @guess_history = []
   end
 
-  def test_reveal_correct_guess_reveals_all_correct_letters
-    assert_equal %w{p _ p p _ _ _ _ _ _}, @teacher.reveal_correct_guess(@secret_word, @revealed_word, @user_input)
+  def test_store_history
+    assert_equal %w{ p }, @teacher.store_history(@guess_history, @user_input)
   end
-  
+
+  # def test_reveal_correct_guess_reveals_all_correct_letters
+  #   assert_equal %w{p _ p p _ _ _ _ _ _}, @teacher.reveal_correct_guess(@secret_word, @revealed_word, @user_input, @guess_history)
+  # end
+
 end
