73e19d5fa48528d68105dc014ccd32f7bd61f876
Jeffrey Bartels <jeffreybartels@gmail.com>
2017-08-22 19:35:32 +1000

    Rename method

--- lib/player.rb
+++ lib/player.rb
@@ -1,16 +1,16 @@
-class Player 
+class Player
   attr_accessor :guess_history, :turns
-  
+
   def initialize
     @guess_history = Array.new
     @turns = 10
   end
-  
+
   def winning_conditions_met?(secret_word, revealed_word)
     secret_word.split("") == revealed_word
   end
-  
-  def load_previous_game?
+
+  def request_previous_game?
     puts "Would you like to load a previous game? (yes/no)"
     choice = gets.chomp
     case choice
