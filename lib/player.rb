70ddbc44074c4a2cb61650212a6e9a723cb360e5
Jeffrey Bartels <jeffreybartels@gmail.com>
2017-09-28 19:50:58 +1000

    Minor fixes to Player class

--- lib/player.rb
+++ lib/player.rb
@@ -2,30 +2,29 @@ class Player
   attr_accessor :guess_history, :turns
 
   def initialize
-    @guess_history = Array.new
+    @guess_history = []
     @turns = 10
   end
 
   def winning_conditions_met?(secret_word, revealed_word)
-    secret_word.split("") == revealed_word
+    secret_word.split('') == revealed_word
   end
 
   def request_previous_game?
     begin
-      puts "Would you like to load a previous game? (yes/no)"
+      puts 'Would you like to load a previous game? (yes/no)'
       choice = gets.chomp
       case choice
-        when "yes"
-          true
-        when "no"
-          false
-        else
-          puts "Please type 'yes' or 'no'."
-          raise
-        end
+      when 'yes'
+        true
+      when 'no'
+        false
+      else
+        puts 'Please type \'yes\' or \'no\'.'
+        raise
+      end
     rescue
       retry
     end
-
   end
 end
